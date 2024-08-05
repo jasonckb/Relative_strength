ae025ae20a30bd0d876849f42e2dd94b8b141588
jasonckb <122160477+jasonckb@users.noreply.github.com>
2024-08-05 23:04:12 +0800

    Add files via upload

--- streamlit_RS.py
+++ streamlit_RS.py
@@ -114,7 +114,7 @@ table = ax.table(cellText=table_data, cellLoc='center', loc='center')
 # Style the table
 table.auto_set_font_size(False)
 table.set_fontsize(14)
-table.scale(2, 4)
+table.scale(2.5, 3)
 
 # Color coding for cells
 for (row, col), cell in table.get_celld().items():
